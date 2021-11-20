c607943740decfe444d2ebd07be195e304f5d69b
ggwp <ggwp@tutanota.com>
2021-11-21 02:21:19 +0700

    - Remove unused code

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,3 @@
-
-chrome.action.onClicked.addListener(tab => {
-    alert(`Clicked icon on tab ` + tab.id);
-});
-
 chrome.runtime.sendMessage({method: 'toggle'}, value => {
     document.getElementById('status').innerText = 'Dictionary Is ' + (value ? 'Enabled' : 'Disabled');
 })
